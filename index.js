cd3a383311f38df544e22100e0557e7c8ec635de
Ana Gutierrez Jordano <40721795+anagutjor@users.noreply.github.com>
2019-01-03 22:13:46 +0100

    use different variable names for every case

--- index.js
+++ index.js
@@ -6,25 +6,25 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
     
     switch(this.id){
       case "w":
-        var drum_sound = new Audio("sounds/crash.mp3").play();
+        var crash = new Audio("sounds/crash.mp3").play();
         break;
       case "a":
-        var drum_sound = new Audio("sounds/kick-bass.mp3").play();
+        var kickbass = new Audio("sounds/kick-bass.mp3").play();
         break;
       case "s":
-        var drum_sound = new Audio("sounds/snare.mp3").play();
+        var snare = new Audio("sounds/snare.mp3").play();
         break;
       case "d":
-        var drum_sound = new Audio("sounds/tom-1.mp3").play();
+        var tom1 = new Audio("sounds/tom-1.mp3").play();
         break;
       case "j":
-        var drum_sound = new Audio("sounds/tom-2.mp3").play();
+        var tom2 = new Audio("sounds/tom-2.mp3").play();
         break;
       case "k":
-        var drum_sound = new Audio("sounds/tom-3.mp3").play();
+        var tom3 = new Audio("sounds/tom-3.mp3").play();
         break;
       case "l":
-        var drum_sound = new Audio("sounds/tom-4.mp3").play();
+        var tom4 = new Audio("sounds/tom-4.mp3").play();
         break;
       default:
         break;
