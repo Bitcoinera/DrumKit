09a03c8561506c925e12c7e1538183ec9f9fd4e9
Ana Gutierrez Jordano <40721795+anagutjor@users.noreply.github.com>
2019-01-03 22:09:10 +0100

    separate lines to be more readable

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
+  
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    
     this.style.color = "red";
+    
     switch(this.id){
       case "w":
         var drum_sound = new Audio("sounds/crash.mp3").play();
