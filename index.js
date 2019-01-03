e41fc19e5a2b713a8fb52a9186987851d1369afb
papiloma <40721795+papiloma@users.noreply.github.com>
2019-01-03 22:00:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    this.style.color = "red";
+    switch(this.id){
+      case "w":
+        var drum_sound = new Audio("sounds/crash.mp3").play();
+        break;
+      case "a":
+        var drum_sound = new Audio("sounds/kick-bass.mp3").play();
+        break;
+      case "s":
+        var drum_sound = new Audio("sounds/snare.mp3").play();
+        break;
+      case "d":
+        var drum_sound = new Audio("sounds/tom-1.mp3").play();
+        break;
+      case "j":
+        var drum_sound = new Audio("sounds/tom-2.mp3").play();
+        break;
+      case "k":
+        var drum_sound = new Audio("sounds/tom-3.mp3").play();
+        break;
+      case "l":
+        var drum_sound = new Audio("sounds/tom-4.mp3").play();
+        break;
+      default:
+        break;
+    }      
+  })
+}
+
+
+
