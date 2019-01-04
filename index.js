d0d1eedb31a59030e9324d72fc6e6ecd993b866a
Ana Gutierrez Jordano <40721795+anagutjor@users.noreply.github.com>
2019-01-04 13:57:09 +0100

    adding keyboard functionality

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+//when clicking buttons each drum sound is played
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
-  
+
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    
+  
     this.style.color = "red";
-    
+  
     switch(this.id){
       case "w":
         var crash = new Audio("sounds/crash.mp3").play();
@@ -32,5 +33,33 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++){
   })
 }
 
+//when pressing every key matching the buttons letters the corresponding drum sound is played
+document.addEventListener("keypress", function(e){
 
-
+  switch(e.key){
+    case "w":
+      var crash = new Audio("sounds/crash.mp3").play();
+      break;
+    case "a":
+      var kickbass = new Audio("sounds/kick-bass.mp3").play();
+      break;
+    case "s":
+      var snare = new Audio("sounds/snare.mp3").play();
+      break;
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3").play();
+      break;
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3").play();
+      break;
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3").play();
+      break;
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3").play();
+      break;
+    default:
+      alert("No valid key was pressed. Press one matching a button");
+      break;
+  }      
+})
